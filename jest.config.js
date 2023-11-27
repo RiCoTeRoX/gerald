1f2582a0395f8cc98376c9f97247c89129f41218
Danny Ralde <danny@n1u.app>
2023-11-27 09:37:56 -0300

    [Task-1] config test's

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+const config = {
   preset: 'react-native',
-};
+  testMatch: ['**/*.test.js?(x)', '**/*.test.ts?(x)'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', './jestSetup.js'],
+  transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js', './jestSetup.js'],
+}
+
+module.exports = config
---
+++ jestSetup.js
@@ -0,0 +1,30 @@
+// jest
+// include this line for mocking react-native-gesture-handler
+import 'react-native-gesture-handler/jestSetup'
+
+// include this section and the NativeAnimatedHelper section for mocking react-native-reanimated
+jest
+  .mock('react-native-reanimated', () => {
+    const Reanimated = require('react-native-reanimated/mock')
+
+    // The mock for `call` immediately calls the callback which is incorrect
+    // So we override it with a no-op
+    Reanimated.default.call = () => {}
+
+    return Reanimated
+  })
+
+  // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+  .mock('react-native/Libraries/Animated/NativeAnimatedHelper')
+
+  .mock('react-native-safe-area-context', () => {
+    const inset = { top: 0, right: 0, bottom: 0, left: 0 }
+
+    return {
+      ...jest.requireActual('react-native-safe-area-context'),
+      SafeAreaProvider: jest.fn(({ children }) => children),
+      SafeAreaConsumer: jest.fn(({ children }) => children(inset)),
+      useSafeAreaInsets: jest.fn(() => inset),
+      useSafeAreaFrame: jest.fn(() => ({ x: 0, y: 0, width: 390, height: 844 })),
+    }
+  })
